74913e59b81c9ed57eea8e9e76faaead54f28315
Alberto Sartori <alberto.sartori.as@gmail.com>
2022-06-01 17:27:13 +0200

    fix: discriminator

--- test/plug/cast_test.exs
+++ test/plug/cast_test.exs
@@ -245,7 +245,8 @@ defmodule OpenApiSpex.Plug.CastTest do
                  "id" => 1234,
                  "inserted_at" => nil,
                  "name" => "asdf",
-                 "updated_at" => "2017-09-12T14:44:55Z"
+                 "updated_at" => "2017-09-12T14:44:55Z",
+                 "age" => nil
                }
              }
 
